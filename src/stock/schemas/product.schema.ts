db036ebedc481f52169f6dbeeb7e67ff1305530b
João Gabriel Santana <joao.santana09@outlook.com>
2025-03-30 21:11:36 -0300

    :bug: fix: Fix type of quantity field in Product Schema

--- src/stock/schemas/product.schema.ts
+++ src/stock/schemas/product.schema.ts
@@ -4,21 +4,21 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 import { IProduct } from './models/product.interface';
 
-export type ProductDocument = HydratedDocument<Product>
+export type ProductDocument = HydratedDocument<Product>;
 
 @Schema()
-export class Product implements IProduct{
-    @Prop({type: mongoose.Schema.Types.ObjectId})
-    id?: string | undefined;
+export class Product implements IProduct {
+  @Prop({ type: mongoose.Schema.Types.ObjectId })
+  id?: string | undefined;
 
-    @Prop()
-    name: string;
-    
-    @Prop()
-    quantity?: number | undefined;
-    
-    @Prop()
-    relationId: number;
+  @Prop()
+  name: string;
+
+  @Prop()
+  quantity?: number;
+
+  @Prop()
+  relationId: number;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
